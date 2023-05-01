5f5f716b3406fde844fd34a39e5d13ae4e212499
Frances Okolo <45973205+FrancesOkolo@users.noreply.github.com>
2023-05-01 01:15:39 +0100

    Update index html
    Update index html with embedded Tableau API

---
+++ index.js
@@ -0,0 +1,5 @@
+
+Spry.Utils.addLoadListener(function() {
+                    var divElement = document.getElementById('viz1682898624511');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1500px';vizElement.style.height='927px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+
+});
