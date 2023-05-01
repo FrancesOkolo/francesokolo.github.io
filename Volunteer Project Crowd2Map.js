70f553bacb073ab051be1ee988e7043f4136f5df
Frances Okolo <45973205+FrancesOkolo@users.noreply.github.com>
2023-05-01 02:40:13 +0100

    Updates

---
+++ Volunteer Project Crowd2Map.js
@@ -0,0 +1,5 @@
+
+Spry.Utils.addLoadListener(function() {
+ var divElement = document.getElementById("viz1682903859948");                    var vizElement = divElement.getElementsByTagName("object")[0];                    vizElement.style.width="1100px";vizElement.style.height="2497px";                    var scriptElement = document.createElement("script");                    scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+
+});
